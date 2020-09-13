0204201dda545647b5fabdb66ee767c4282ef282
Eduard Faus <eduardfauspublic@gmail.com>
2020-09-13 14:35:35 -0400

    pls help me

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ Components.forEach((item, i) => {
   });
 });
 //PORT ENVIRONMENT VARIABLE
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}..`));
